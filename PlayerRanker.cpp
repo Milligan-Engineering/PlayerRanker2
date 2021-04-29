9d107211daee7f5d6817273f8b5d414e8bfc8b34
jcnickens@my.milligan.edu <jcnickens@my.milligan.edu>
2021-04-29 15:43:29 -0400

    Final Code Note: I fixed the issue with my code ending after incorecct input.

--- PlayerRanker.cpp
+++ PlayerRanker.cpp
@@ -1,14 +1,15 @@
 //File Name: Player Ranker
-//Author: Josh Nickens
+//Author: Joshua Nickens
 //Email Address: jcnickens@my.milligan.edu
-//Term Project
+//Final Project
 //Description: This program analyzes the stats of players in order to determine a league MVP
-//PM06
-//Last Changed: 3/17/21
+//Final Presentaion
 
 #include <iostream>
 #include <string>
 #include <list>
+#include <fstream>
+#include <valarray>
 using namespace std;
 
 int choice;
@@ -19,48 +20,28 @@ string player[maxPlayers];
 int points[maxPlayers];
 int rebounds[maxPlayers];
 int fouls[maxPlayers];
-double ppg[maxPlayers]; 
+int weekPoint[16];
+int weeklyPlayer[60][16][16][16];
+double ppg[maxPlayers];
+//ppg stands for points per game
 double rpg[maxPlayers];
+//rpg stands for rebounds per game
 double fpg[maxPlayers];
-void listPrint(string s[], int size);
-void listPrint(string s[], int p[], int size);
+//fpg stands for fouls per game
 
-
-//gamesTotal will be constant for all teams barring any cancelations or forfeits
-//double mvpCalc()
-//Precondition: given player info
-//Postcondition: output index of MVP points
-//int sum(points[]);
-//Precondition: Number of games, int point scored each game
-//Postcondition: Sum of points
-//float average(points1);
-//Precondition: Total points, number of games
-//Postcondition: Average points per game
-
+struct node
+{
+	string player[maxPlayers];
+	int points[maxPlayers];
+	int rebounds[maxPlayers];
+	int fouls[maxPlayers];
+	node* next;
+};
 
 int main()
 {
 	cout << "Welcome to the Player Ranker \n";
-	cout << "Are you starting a new program or continuing an old one?\n";
-	cout << "1. New program   2. Old program";
-	cout << " :";
-	cin >> choice;
-	switch (choice)
-	{
-	case 1:
-		cout << "Starting new program.";
-		break;
-
-	case 2:
-		cout << "Continuing old program.";
-		break;
-
-	case 3:
-		cout << "Not a valid choice.";
-		cout << "Choose again.";
-		cin >> choice;
-		break;
-	}
+	cout << "Starting new program.";
 	cout << "Do you have total points or by game points?\n";
 	cout << "1. Total points   2. By game points";
 	cout << " :";
@@ -68,88 +49,91 @@ int main()
 	switch (choice)
 	{
 	case 1:
+	{	cout << "Starting total point system\n";
+	cout << "How many players are there? ";
+	cin >> numPlayers;
+	while (numPlayers < 0 || numPlayers > 60)
 	{
-		cout << "Starting total point system\n";
+		cout << "Please re-enter number of players.";
+		cin >> numPlayers;
+	}
+
+	for (int i = 0; i < numPlayers; i++)
+	{
+		cout << "\nWhat is the player's name? ";
+		cin >> player[i];
+		cout << "How many points did they score? ";
+		cin >> points[i];
+		cout << "How many rebounds did they have? ";
+		cin >> rebounds[i];
+		cout << "How many times did they foul? ";
+		cin >> fouls[i];
+		ppg[i] = points[i] / gamesTotal;
+		rpg[i] = rebounds[i] / gamesTotal;
+		fpg[i] = fouls[i] / gamesTotal;
+		cout << player[i] << "'s stats are: \n";
+		cout << "Points per Game: ";
+		cout << ppg[0] << " ";
+		cout << "\nRebounds per Game: ";
+		cout << rpg[0] << " ";
+		cout << "\nFouls per Game: ";
+		cout << fpg[0] << " ";
+	}
+	return 0;
+	}
+	case 2:
+	{
+		cout << "Starting by game system \n";
 		cout << "How many players are there? ";
 		cin >> numPlayers;
 		if (numPlayers < 0 || numPlayers > 60)
 		{
 			cout << "Please re-enter the number of players.";
 			cin >> numPlayers;
-		}
-		for (int i = 0; i <= numPlayers; i++)
-		{
-			cout << "\nWhat is the player's name? ";
-			cin >> player[i];
-			cout << "How many points did they score? ";
-			cin >> points[i];
-			cout << "How many rebounds did they have? ";
-			cin >> rebounds[i];
-			cout << "How many times did they foul? ";
-			cin >> fouls[i];
 		}
 		for (int i = 0; i < numPlayers; i++)
 		{
-			ppg[i] = points[i] / gamesTotal;
-			rpg[i] = rebounds[i] / gamesTotal;
-			fpg[i] = fouls[i] / gamesTotal;
-			cout << "player1 stats are: \n";
-			cout << "Points per Game: ";
-			cin >> ppg[0];
-			cout << "\nRebounds per Game: ";
-			cin >> rpg[0];
-			cout << "\nFouls per Game: ";
-			cin >> fpg[0];
-		}
-		break;
-	}
-
-
-
-	case 2:
-	{
-		cout << "Starting by game system";
-		cout << "How many players are there? ";
-		cin >> numPlayers;
-		if (numPlayers < 0 || numPlayers > 60)
-		{
-			cout << "Please re-enter the number of players.";
-			cin >> numPlayers;
-		}
-		for (int i = 0; i <= numPlayers; i++)
-		{
 			cout << "\nWhat is the player's name? ";
 			cin >> player[i];
-			cout << "Input their points for each game. ";
-			break;
+			cout << "Input their points for each game. \n";
+			for (int k = 0; k <= 15; k++)
+			{
+				cin >> weekPoint[k];
+			}
+			valarray<int> weekPoint[15];
+
+
+		}
+	}
+	case 3:
+	{
+		while (choice != 1 && choice != 2)
+		{
+			cout << "Not a valid choice.";
+			cout << "Choose again.";
+			cin >> choice;
+		}
+	}
+	}
+}
+
+void printList(node* n)
+{
+	while (n != NULL)
+	{
+		for (int j = 0; j <= numPlayers; j++)
+		{
+			cout << n->player[j] << " ";
+			cout << n->points[j] << " ";
+			cout << n->rebounds[j] << " ";
+			cout << n->fouls[j] << " ";
+			n = n->next;
 		}
 	}
 
-
-	case 3:
-	{
-		cout << "Not a valid choice.";
-		cout << "Choose again.";
-		cin >> choice;
-		break;
-	}
-
-
-
-	//int sum(week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16);
-	//{
-		//cin week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16;
-		//sum(week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16);
-	//}
-	//float average(week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16);
-	//{
-		//cin week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16;
-		//average(week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16);
-	//}
-	return 0;
-
-	}
 }
+
+
 void listPrint(string s[], int size)
 {
 	for (int i = 0; i < size; i++)
